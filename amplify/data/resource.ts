018f5ab5148269bd42e548ecb57e8f7358e31807
Jorge Donoso <jdevcl@gmail.com>
2026-09-17 11:24:07 -0700

    Add Profile model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,7 +3,42 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   Profile: a
     .model({
+      image: a.string().required(),
+
+      // Basic
       gender: a.string(),
+      age: a.float(),
+      ethnicity: a.string(),
+
+      // Hair
+      hairColor: a.string(),
+      hairLength: a.string(),
+      bald: a.float(),
+
+      // Eyes
+      eyeColor: a.string(),
+
+      // Face
+      smile: a.float(),
+
+      // Makeup
+      eyeMakeup: a.float(),
+      lipMakeup: a.float(),
+
+      // Emotions
+      happiness: a.float(),
+      sadness: a.float(),
+      anger: a.float(),
+      surprise: a.float(),
+      neutral: a.float(),
+
+      // Facial hair
+      moustache: a.float(),
+      beard: a.float(),
+      sideburns: a.float(),
+
+      // Confidence / quality
+      bugProbability: a.float(),
     })
     .authorization((allow) => [allow.guest()]),
 });
